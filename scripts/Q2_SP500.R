cbdfdb5fd105c127acc639cc401f4400b8d577b3
beatriz-ls <beatrizlimasilveira@gmail.com>
2024-11-27 20:51:14 -0300

    finalizando ex 2 e 3

--- scripts/Q2_SP500.R
+++ scripts/Q2_SP500.R
@@ -26,3 +26,48 @@ colSums(is.na(data))
 datas_completas <- seq.Date(min(data$date), max(data$date), by = "day")
 datas_faltantes <- as.Date(setdiff(datas_completas, data$date),
                            format = "%Y-%m-%d")
+
+##### calculo do retorno médio anual -------------------------------------------
+
+
+n_anos <- as.numeric(difftime(max(data$date), min(data$date), units = "days")) / 365
+
+valor_inicial <- first(data$adjusted)
+valor_final <- last(data$adjusted)
+
+retorno_medio_anual <- (valor_final / valor_inicial)^(1 / n_anos) - 1
+
+##### calculo do retorno simples médio anualizado ------------------------------
+
+data <- data %>%
+  arrange(date) %>%
+  mutate(retorno_diario = (adjusted / lag(adjusted)) - 1)
+
+data <- data %>% filter(!is.na(retorno_diario))
+
+media_retorno_diario <- mean(data$retorno_diario)
+
+retorno_simples_anualizado <- media_retorno_diario * 252
+
+##### simulando investimento ---------------------------------------------------
+
+# Se vc investir R/. 1.00 no ativo no final de Dezembro de 1994, qual é o valor do
+# investimento ao final do Dezembro de 2001?
+
+preco_inicial <- data %>% 
+  filter(date == as.Date("1994-12-30")) %>% 
+  pull(adjusted)
+
+preco_final <- data %>% 
+  filter(date == as.Date("2001-12-31")) %>% 
+  pull(adjusted)
+
+investimento_inicial <- 1
+valor_final_investimento <- investimento_inicial * (preco_final / preco_inicial)
+
+
+
+
+
+
+
--- scripts/Q3_PETR4.R
+++ scripts/Q3_PETR4.R
@@ -13,3 +13,36 @@ data <- tq_get("^GSPC",
 data <- data %>% 
   select(-symbol) %>%
   mutate(date = as.Date(date, format = "%Y-%m-%d"))
+
+# checagem dos dados
+
+any(is.na(data))
+
+sum(is.na(data))
+
+colSums(is.na(data))
+
+datas_completas <- seq.Date(min(data$date), max(data$date), by = "day")
+datas_faltantes <- as.Date(setdiff(datas_completas, data$date),
+                           format = "%Y-%m-%d")
+
+##### analises de log retorno --------------------------------------------------
+
+data <- data %>%
+  arrange(date) %>%
+  mutate(
+    retorno_simples = (adjusted / lag(adjusted)) - 1,
+    log_retorno = log(adjusted / lag(adjusted))
+  )
+
+# (a) retorno simples
+retorno_dia1_para_dia2 <- data$retorno_simples[2]
+retorno_dia1_para_dia7 <- (data$adjusted[7] / data$adjusted[1]) - 1
+
+# (b) logretornos
+logretorno_dia5_para_dia6 <- log(data$adjusted[6] / data$adjusted[5])
+logretorno_dia5_para_dia10 <- log(data$adjusted[10] / data$adjusted[5])
+
+# (c) autocorrelação
+logretornos <- na.omit(data$log_retorno)
+acf_logretornos <- acf(logretornos, main = "Função de Autocorrelação dos Logretornos do PETR4")
