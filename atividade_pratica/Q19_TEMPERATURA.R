a75b901fef82274f4fbc7929532a42216e201fe1
beatriz-ls <beatrizlimasilveira@gmail.com>
2025-01-26 21:46:52 -0300

    Q19 modificações

--- atividade_pratica/Q19_TEMPERATURA.R
+++ atividade_pratica/Q19_TEMPERATURA.R
@@ -14,4 +14,32 @@ data <- read_ods("atividade_pratica/temperatura.ods")
 
 data1 <- ts(data$Ubatuba,  start = c(1976, 1), frequency = 12)
 
+# checagem dos dados
 
+any(is.na(data))
+
+sum(is.na(data))
+
+colSums(is.na(data))
+
+##### decomposição -------------------------------------------------------------
+
+decompose <- decompose(data1)
+
+plot(decompose)
+
+stl <- stl(data1, s.window = "periodic")
+
+plot(stl)
+
+mean(data1)
+
+data2 <- data1 - mean(data1)
+
+decompose <- decompose(data2)
+
+plot(decompose)
+
+stl <- stl(data2, s.window = "periodic")
+
+plot(stl)
