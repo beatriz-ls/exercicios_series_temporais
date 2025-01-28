69db59a482aae6500d0926f57857c798d70a8af8
beatriz-ls <beatrizlimasilveira@gmail.com>
2025-01-28 19:31:28 -0300

    Atualização Q19

--- atividade_pratica/Q19_TEMPERATURA.R
+++ atividade_pratica/Q19_TEMPERATURA.R
@@ -5,6 +5,7 @@ library(tidyr)
 library(ggplot2)
 library(tseries)
 library(readODS)
+library(tidyverse)
 
 ##### baixando dados -----------------------------------------------------------
 
@@ -12,7 +13,7 @@ data <- read_ods("atividade_pratica/temperatura.ods")
 
 # transformando em formato time series
 
-data1 <- ts(data$Ubatuba,  start = c(1976, 1), frequency = 12)
+data_ts <- ts(data$Ubatuba,  start = c(1976, 1), frequency = 12)
 
 # checagem dos dados
 
@@ -24,22 +25,62 @@ colSums(is.na(data))
 
 ##### decomposição -------------------------------------------------------------
 
-decompose <- decompose(data1)
+decompose <- decompose(data_ts)
 
 plot(decompose)
 
-stl <- stl(data1, s.window = "periodic")
+stl <- stl(data_ts, s.window = "periodic")
 
 plot(stl)
 
-mean(data1)
+# retirando a constante para melhor visualização
 
-data2 <- data1 - mean(data1)
+mean(data_ts)
 
-decompose <- decompose(data2)
+data_ts1 <- data_ts - mean(data_ts)
 
-plot(decompose)
+decompose1 <- decompose(data_ts1)
 
-stl <- stl(data2, s.window = "periodic")
+plot(decompose1)
 
-plot(stl)
+stl1 <- stl(data_ts1, s.window = "periodic")
+
+plot(stl1)
+
+
+##### modelo de decomposição ---------------------------------------------------
+
+data_fit <- data %>% select(-c(Cananeia))
+
+## teste de estacionariedade
+
+adf.test(data_fit$Ubatuba) # série estacionária
+
+## assumindo modelo com tendencia não consideravel e sazonalidade deterministica
+
+# Converter ano para numérico e criar variável de tempo
+data_fit$t <- 1:nrow(data_df)
+data_fit$t2 <- data_df$t^2
+
+# ajustar o modelo de regressão linear
+modelo <- lm(Ubatuba ~ factor(Mes), data = data_fit)
+
+# Exibir os resultados do modelo
+summary(modelo)
+
+## analisando os residuos do modelo proposto
+
+# Diagnóstico gráfico dos resíduos
+par(mfrow = c(2,2))
+plot(modelo)
+
+# Teste de Ljung-Box para verificar autocorrelação nos resíduos
+Box.test(residuals(modelo), lag = 12, type = "Ljung-Box")
+
+
+modelo_hw <- HoltWinters(serie_ts, beta = FALSE)
+
+
+
+
+
